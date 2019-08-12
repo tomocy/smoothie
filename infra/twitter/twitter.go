5b4c83949a27d6b27ac30c932ffc2901753f55c3
tomocy <tomocy.dev@gmail.com>
2019-08-13 02:41:48 +0900

    add Tweet
    to represent a tweet returned by twitter

---
+++ infra/twitter/twitter.go
@@ -0,0 +1,23 @@
+package twitter
+
+import "time"
+
+type Tweet struct {
+	ID        string `json:"id_str"`
+	Text      string `json:"text"`
+	FullText  string `json:"full_text"`
+	CreatedAt date   `json:"created_at"`
+}
+
+type date time.Time
+
+func (d *date) UnmarshalJSON(data []byte) error {
+	withoutQuotes := (string(data))[1 : len(data)-1]
+	parsed, err := time.ParseInLocation(time.RubyDate, withoutQuotes, time.UTC)
+	if err != nil {
+		return err
+	}
+	*d = date(parsed.In(time.Local))
+
+	return nil
+}
