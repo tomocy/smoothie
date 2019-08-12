2c197a49787315454234e2b5261b76c89c29132c
tomocy <tomocy.dev@gmail.com>
2019-08-13 04:46:46 +0900

    modify main
    to run generated Runner

--- cmd/smoothie/main.go
+++ cmd/smoothie/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/tomocy/smoothie/cmd/smoothie/runner"
+)
+
+func main() {
+	r := runner.New()
+	if err := r.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to run: %s\n", err)
+		os.Exit(1)
+	}
+}
