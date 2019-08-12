d61f77b2c07db22feec9923a14c6bd771e3b9004
tomocy <tomocy.dev@gmail.com>
2019-08-13 05:10:57 +0900

    fix

--- domain/model.go
+++ domain/model.go
@@ -15,7 +15,7 @@ type Posts []*Post
 func (ps *Posts) SortByNewest() {
 	sort.Slice(*ps, func(i, j int) bool {
 		return (*ps)[i].CreatedAt.After((*ps)[j].CreatedAt)
-	})a
+	})
 }
 
 type Post struct {
