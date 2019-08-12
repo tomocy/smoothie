48a701bea84163dae81665cc56e25b0589618216
tomocy <tomocy.dev@gmail.com>
2019-08-13 02:35:24 +0900

    add NewTwitter
    to generate new Twitter

--- infra/twitter.go
+++ infra/twitter.go
@@ -2,6 +2,20 @@ package infra
 
 import "github.com/garyburd/go-oauth/oauth"
 
+func NewTwitter(id, secret string) *Twitter {
+	return &Twitter{
+		oauthClient: oauth.Client{
+			TemporaryCredentialRequestURI: "https://api.twitter.com/oauth/request_token",
+			ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authenticate",
+			TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
+			Credentials: oauth.Credentials{
+				Token:  id,
+				Secret: secret,
+			},
+		},
+	}
+}
+
 type Twitter struct {
 	oauthClient oauth.Client
 }
