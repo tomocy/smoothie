0677ca345f5a065c62b15887e0b3438856120e49
tomocy <tomocy.dev@gmail.com>
2019-08-13 04:28:42 +0900

    add (*cli).ShowPosts
    to show posts in cli

--- cmd/smoothie/runner/mode.go
+++ cmd/smoothie/runner/mode.go
@@ -1,5 +1,15 @@
 package runner
 
+import (
+	"os"
+
+	"github.com/tomocy/smoothie/domain"
+)
+
 type cli struct {
 	printer printer
+}
+
+func (c *cli) ShowPosts(ps domain.Posts) {
+	c.printer.PrintPosts(os.Stdout, ps)
 }
