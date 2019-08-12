836fc352430ec09127c3d2032a68184b031c88e0
tomocy <tomocy.dev@gmail.com>
2019-08-13 03:07:33 +0900

    add (*Twitter).retrieveAuthorization
    to retrieve authorization from twitter

--- infra/twitter.go
+++ infra/twitter.go
@@ -1,6 +1,11 @@
 package infra
 
-import "github.com/garyburd/go-oauth/oauth"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/garyburd/go-oauth/oauth"
+)
 
 func NewTwitter(id, secret string) *Twitter {
 	return &Twitter{
@@ -18,4 +23,26 @@ func NewTwitter(id, secret string) *Twitter {
 
 type Twitter struct {
 	oauthClient oauth.Client
+}
+
+func (t *Twitter) retreiveAuthorization() (*oauth.Credentials, error) {
+	temp, err := t.oauthClient.RequestTemporaryCredentials(http.DefaultClient, "", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return t.requestClientAuthorization(temp)
+}
+
+func (t *Twitter) requestClientAuthorization(temp *oauth.Credentials) (*oauth.Credentials, error) {
+	url := t.oauthClient.AuthorizationURL(temp, nil)
+	fmt.Println("open this url: ", url)
+
+	fmt.Print("PIN: ")
+	var pin string
+	fmt.Scanln(&pin)
+
+	token, _, err := t.oauthClient.RequestToken(http.DefaultClient, temp, pin)
+
+	return token, err
 }
