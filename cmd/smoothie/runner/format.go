1d4ba16d148b0e856d188e20e9623af877037f93
tomocy <tomocy.dev@gmail.com>
2019-08-14 22:24:01 +0900

    modify (*text).printPost
    to print the created time of the given post in 2006/01/02

--- cmd/smoothie/runner/format.go
+++ cmd/smoothie/runner/format.go
@@ -5,7 +5,6 @@ import (
 	"io"
 
 	"github.com/tomocy/smoothie/domain"
-	"github.com/tomocy/tago"
 )
 
 type text struct {
@@ -25,9 +24,8 @@ func (t *text) PrintPosts(w io.Writer, ps domain.Posts) {
 }
 
 func (t *text) printPost(w io.Writer, p *domain.Post) {
-	without := tago.NewWithout(tago.DefaultDuration, "2006/01/02")
 	fmt.Fprintf(
 		w, "(%s) %s @%s %s\n%s\n",
-		p.Driver, p.User.Name, p.User.Username, without.Ago(p.CreatedAt), p.Text,
+		p.Driver, p.User.Name, p.User.Username, p.CreatedAt.Format("2006/01/02"), p.Text,
 	)
 }
