e3411ff01886b143783f203d2c8342d4471b80d7
tomocy <tomocy.dev@gmail.com>
2019-08-14 23:51:25 +0900

    add Post
    to represent a post returned from reddit

---
+++ infra/reddit/reddit.go
@@ -0,0 +1,46 @@
+package reddit
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Post struct {
+	Name                  string        `json:"name"`
+	SubredditNamePrefixed string        `json:"subreddit_name_prefixed"`
+	Author                string        `json:"author"`
+	Title                 string        `json:"title"`
+	SelfText              string        `json:"selftext"`
+	CreatedUTC            unixTimestamp `json:"created_utc"`
+}
+
+type unixTimestamp time.Time
+
+func (t *unixTimestamp) UnmarshalJSON(data []byte) error {
+	parsed, err := t.parse(string(data))
+	if err != nil {
+		return err
+	}
+	*t = unixTimestamp(parsed.Local())
+
+	return nil
+}
+
+func (t *unixTimestamp) parse(ts string) (time.Time, error) {
+	splited := strings.Split(ts, ".")
+	if len(splited) != 2 {
+		return time.Time{}, errors.New("invalid format of unix timestamp: the format should be sec.nsec")
+	}
+	sec, err := strconv.ParseInt(splited[0], 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+	nsec, err := strconv.ParseInt(splited[1], 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(sec, nsec), nil
+}
