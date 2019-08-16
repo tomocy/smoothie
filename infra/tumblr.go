76fbb3452687bd06d90933aeb877ba0b01c81f5a
tomocy <tomocy.dev@gmail.com>
2019-08-16 23:55:04 +0900

    add (*Tumblr).retreiveAuthorization
    to retreive authorization from tumblr

--- infra/tumblr.go
+++ infra/tumblr.go
@@ -2,6 +2,8 @@ package infra
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
 	"github.com/garyburd/go-oauth/oauth"
 	"github.com/tomocy/deverr"
@@ -33,4 +35,25 @@ func (t *Tumblr) StreamPostsOfDrivers(ctx context.Context) (<-chan domain.Posts,
 
 func (t *Tumblr) FetchPosts() (domain.Posts, error) {
 	return nil, deverr.NotImplemented
+}
+
+func (t *Tumblr) retreiveAuthorization() (*oauth.Credentials, error) {
+	temp, err := t.oauthClient.RequestTemporaryCredentials(http.DefaultClient, "", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return t.requestClientAuthorization(temp)
+}
+
+func (t *Tumblr) requestClientAuthorization(temp *oauth.Credentials) (*oauth.Credentials, error) {
+	url := t.oauthClient.AuthorizationURL(temp, nil)
+	fmt.Printf("open this url: %s\n", url)
+
+	fmt.Print("PIN: ")
+	var pin string
+	fmt.Scan(&pin)
+
+	token, _, err := t.oauthClient.RequestToken(http.DefaultClient, temp, pin)
+	return token, err
 }
