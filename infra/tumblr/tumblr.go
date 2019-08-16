73ebccf24d6e163ee9d8821d323d7cd7ba8eb0d6
tomocy <tomocy.dev@gmail.com>
2019-08-17 00:45:48 +0900

    add (*Post).Adapt
    to adapt tumblr.Post to domain.Post

--- infra/tumblr/tumblr.go
+++ infra/tumblr/tumblr.go
@@ -1,10 +1,32 @@
 package tumblr
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/tomocy/smoothie/domain"
+)
 
 type Post struct {
-	ID      string    `json:"id"`
-	Summary string    `json:"summary"`
-	Tags    []string  `json:"tags"`
-	Date    time.Time `json:"date"`
+	ID       string    `json:"id"`
+	BlogName string    `json:"blog_name"`
+	Summary  string    `json:"summary"`
+	Tags     []string  `json:"tags"`
+	Date     time.Time `json:"date"`
+}
+
+func (p *Post) Adapt() *domain.Post {
+	return &domain.Post{
+		ID: p.ID,
+		User: &domain.User{
+			Name: p.BlogName,
+		},
+		Text:      p.joinText(),
+		CreatedAt: p.Date,
+	}
+}
+
+func (p *Post) joinText() string {
+	return fmt.Sprintf("%s\n%s", p.Summary, strings.Join(p.Tags, " "))
 }
