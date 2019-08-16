a6a9a0144af8ee7d331c439644c5c4668329ff24
tomocy <tomocy.dev@gmail.com>
2019-08-16 23:38:15 +0900

    make Tumblr implement PostRepo
    but the implementations are empty

--- infra/tumblr.go
+++ infra/tumblr.go
@@ -1,3 +1,33 @@
 package infra
 
+import (
+	"context"
+
+	"github.com/tomocy/deverr"
+
+	"github.com/tomocy/smoothie/domain"
+)
+
 type Tumblr struct{}
+
+func (t *Tumblr) StreamPostsOfDrivers(ctx context.Context) (<-chan domain.Posts, <-chan error) {
+	psCh, errCh := make(chan domain.Posts), make(chan error)
+	go func() {
+		defer func() {
+			close(psCh)
+			close(errCh)
+		}()
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			errCh <- deverr.NotImplemented
+		}
+	}()
+
+	return psCh, errCh
+}
+
+func (t *Tumblr) FetchPosts() (domain.Posts, error) {
+	return nil, deverr.NotImplemented
+}
