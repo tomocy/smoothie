c12e98a324d17aec6539fdc8fd37f32e989e7fc2
tomocy <tomocy.dev@gmail.com>
2019-08-22 14:32:26 +0900

    make Gmail implement PostRepo
    but the implementation is empty

--- infra/gmail.go
+++ infra/gmail.go
@@ -1,5 +1,34 @@
 package infra
 
+import (
+	"context"
+
+	"github.com/tomocy/deverr"
+
+	"github.com/tomocy/smoothie/domain"
+)
+
 type Gmail struct {
 	oauth oauth2Manager
+}
+
+func (g *Gmail) StreamPosts(ctx context.Context) (<-chan domain.Posts, <-chan error) {
+	psCh, errCh := make(chan domain.Posts), make(chan error)
+	go func() {
+		defer func() {
+			close(psCh)
+			close(errCh)
+		}()
+		select {
+		case <-ctx.Done():
+		default:
+			errCh <- deverr.NotImplemented
+		}
+	}()
+
+	return psCh, errCh
+}
+
+func (g *Gmail) FetchPosts() (domain.Posts, error) {
+	return nil, deverr.NotImplemented
 }
