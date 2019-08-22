3fe4fa313d13ed6aad48494f9f3c066adda6889e
tomocy <tomocy.dev@gmail.com>
2019-08-22 18:42:26 +0900

    modify (*Message).joinText
    to join text if the mime type is text/plain

--- infra/gmail/gmail.go
+++ infra/gmail/gmail.go
@@ -1,7 +1,9 @@
 package gmail
 
 import (
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/tomocy/smoothie/domain"
@@ -29,13 +31,32 @@ func (m *Message) Adapt() *domain.Post {
 		User: &domain.User{
 			Name: header.to,
 		},
-		Text:      fmt.Sprintf("%s %s\n", header.from, header.subject),
+		Text:      m.joinText(header),
 		CreatedAt: time.Unix(0, m.InternalDate*int64(time.Millisecond)),
 	}
 }
 
+func (m *Message) joinText(h *header) string {
+	var b strings.Builder
+	fmt.Fprintf(&b, "%s %s", h.from, h.subject)
+	if h.mime != "text/plain" {
+		return b.String()
+	}
+
+	decoded, err := base64.URLEncoding.DecodeString(m.Payload.Body.Data)
+	if err != nil {
+		return b.String()
+	}
+	b.WriteByte('\n')
+	b.Write(decoded)
+
+	return b.String()
+}
+
 func (m *Message) parseHeader() *header {
-	parsed := new(header)
+	parsed := &header{
+		mime: m.Payload.MimeType,
+	}
 	for _, h := range m.Payload.Headers {
 		switch h.Name {
 		case "Subject":
@@ -51,5 +72,5 @@ func (m *Message) parseHeader() *header {
 }
 
 type header struct {
-	subject, from, to string
+	subject, from, to, mime string
 }
