049ecb432fbcb934350a6dbb9d92334ae5c889ab
tomocy <tomocy.dev@gmail.com>
2019-08-22 14:33:59 +0900

    add (*Gmail).retreiveAuthorization
    to retreive authorization from google

--- infra/gmail.go
+++ infra/gmail.go
@@ -2,10 +2,11 @@ package infra
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/tomocy/deverr"
-
 	"github.com/tomocy/smoothie/domain"
+	"golang.org/x/oauth2"
 )
 
 type Gmail struct {
@@ -31,4 +32,15 @@ func (g *Gmail) StreamPosts(ctx context.Context) (<-chan domain.Posts, <-chan er
 
 func (g *Gmail) FetchPosts() (domain.Posts, error) {
 	return nil, deverr.NotImplemented
+}
+
+func (g *Gmail) retreiveAuthorization() (*oauth2.Token, error) {
+	url := g.oauth.authURL()
+	fmt.Printf("open this link: %s\n", url)
+
+	return g.handleAuthorizationRedirect()
+}
+
+func (g *Gmail) handleAuthorizationRedirect() (*oauth2.Token, error) {
+	return g.oauth.handleRedirect(context.Background(), nil, "/smoothie/gmail/authorization")
 }
