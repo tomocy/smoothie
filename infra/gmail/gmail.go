4e7a5e83b577f24a0840cffbf1a87986646ad99e
tomocy <tomocy.dev@gmail.com>
2019-08-22 17:10:52 +0900

    add (*Message).Adapt
    to adapt to Post

--- infra/gmail/gmail.go
+++ infra/gmail/gmail.go
@@ -1,5 +1,41 @@
 package gmail
 
-import "google.golang.org/api/gmail/v1"
+import (
+	"github.com/tomocy/smoothie/domain"
+	"google.golang.org/api/gmail/v1"
+)
 
 type Message gmail.Message
+
+func (m *Message) Adapt() *domain.Post {
+	header := m.parseHeader()
+	return &domain.Post{
+		ID:     m.Id,
+		Driver: "gmail",
+		User: &domain.User{
+			Name: header.to,
+		},
+		Text: fmt.Sprintf("%s\n%s", header.from, header.subject),
+		CreatedAt: time.Unix(0, m.InternalDate*int64(time.Millisecond))
+	}
+}
+
+func (m *Message) parseHeader() *header {
+	parsed := new(header)
+	for _, h := range m.Payload.Headers {
+		switch h.Name {
+		case "Subject":
+			parsed.subject = h.Value
+		case "From":
+			parsed.from = h.Value
+		case "To":
+			parsed.to = h.Value
+		}
+	}
+
+	return parsed
+}
+
+type header struct {
+	subject, from, to string
+}
