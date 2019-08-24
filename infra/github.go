364ec16e9770785311064222d73f200a5ed6c04b
tomocy <tomocy.dev@gmail.com>
2019-08-25 03:19:04 +0900

    make GitHub implement PostRepo
    but the implementation is empty

--- infra/github.go
+++ infra/github.go
@@ -1,3 +1,34 @@
 package infra
 
+import (
+	"context"
+
+	"github.com/tomocy/deverr"
+
+	"github.com/tomocy/smoothie/domain"
+)
+
 type GitHub struct{}
+
+func (g *GitHub) StreamPosts(ctx context.Context) (<-chan domain.Posts, <-chan error) {
+	psCh, errCh := make(chan domain.Posts), make(chan error)
+	go func() {
+		defer func() {
+			close(psCh)
+			close(errCh)
+		}()
+
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			errCh <- deverr.NotImplemented
+		}
+	}()
+
+	return psCh, errCh
+}
+
+func (g *GitHub) FetchPosts() (domain.Posts, error) {
+	return nil, deverr.NotImplemented
+}
