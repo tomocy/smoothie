b154737fcd0b7ae5a4d73695fb172b7dedb07b0a
tomocy <tomocy.dev@gmail.com>
2019-08-25 03:24:08 +0900

    add Issue
    to represent a issue returned from github api

--- infra/github/github.go
+++ infra/github/github.go
@@ -1,5 +1,15 @@
 package github
 
+import "time"
+
+type Issue struct {
+	ID        int       `json:"id"`
+	User      *User     `json:"user"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 type User struct {
 	Login string `json:"login"`
 }
