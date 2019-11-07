b1caf3317f81fef1369fcacd3b6255dab6b7e698
tomocy <tomocy.dev@gmail.com>
2019-11-07 13:36:38 +0900

    add Item
    to represent an item of Qiita

--- infra/qiita/qiita.go
+++ infra/qiita/qiita.go
@@ -1,5 +1,15 @@
 package qiita
 
+import "time"
+
+type Item struct {
+	ID        string    `json:"id"`
+	User      *User     `json:"user"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 type User struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
